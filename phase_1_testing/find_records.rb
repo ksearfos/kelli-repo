833b0cfe2e1426fa0b7d9b2da6e867110afb426d
Kelli Searfos <ksearfos@manifestcorp.com>
2014-03-03 09:23:06 -0500

    Fixing conflicts from merge with master.

--- phase_1_testing/find_records.rb
+++ phase_1_testing/find_records.rb
@@ -10,7 +10,6 @@ input_path = $proj_dir + "/resources"
 util = Dir.new( util_path )   # all helper functions
 util.entries.each{ |f| require util_path + "/" + f if f.include?( '.rb' ) }
 
-<<<<<<< HEAD
 # FILE = "#{input_path}/manifest_lab_out.txt"
 FILE = "#{input_path}/manifest_lab_short_unix.txt"
 OUT_FILE = "#{$proj_dir}/record_results.txt"
@@ -19,27 +18,16 @@ VERBOSE = true
 msg = get_hl7( FILE )
 all_hl7 = hl7_by_record( msg )
 
-=======
-# FILE = "C:/Users/Owner/Documents/manifest_lab_out.txt"
-FILE = "C:/Users/Owner/Documents/manifest_lab_short_unix.txt"
-OUT_FILE = "C:/Users/Owner/Documents/test_results.txt"
-
-msg = get_hl7( FILE )
-all_hl7 = hl7_by_record( msg )
->>>>>>> master
 pos_comparer = RecordComparer.new( all_hl7 )           # finds all positive test cases
 neg_comparer = RecordComparer.new( all_hl7, false )    # finds all negative test cases, e.g. cases where criterion are not met
 pos_comparer.analyze
 neg_comparer.analyze
-
-<<<<<<< HEAD
 pos_comparer.summarize( VERBOSE )
 neg_comparer.summarize( VERBOSE )
 
 puts "\nSaving results..."
 sleep 1
-=======
->>>>>>> master
+
 res = pos_comparer.people_to_use
 res << neg_comparer.people_to_use
 res.flatten!.uniq!        # some records might both pass criteria and fail them, so get rid of duplicates
@@ -52,8 +40,4 @@ write_str = res.join( "\n" )
 
 File.open( OUT_FILE, "w" ) { |f| f.puts write_str }
 
-<<<<<<< HEAD
 puts "Results can now be found in #{OUT_FILE}."
-=======
-puts "\nResults can be found in #{OUT_FILE}."
->>>>>>> master
