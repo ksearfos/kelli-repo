ab9a60eed8d3472b91c6a98056ff6880577e465c
Kelli Searfos <ksearfos@manifestcorp.com>
2014-03-05 14:57:22 -0500

    Just the module

---
+++ phase_1_testing/hl7module/test.rb
@@ -0,0 +1,24 @@
+#!/bin/env ruby
+
+proj_dir = File.expand_path( "../..", __FILE__ )
+FILE = "#{proj_dir}/resources/manifest_lab_short_unix.txt"
+# FILE = "C:/Users/Owner/Documents/manifest_lab_out_shortened.txt"
+# FILE = "C:/Users/Owner/Documents/manifest_rad_out_shortened.txt"
+$msg = <<END
+0000000599MSH|^~\&|HLAB|RMH|||20140128041144||ORU^R01|201401280411444405|T|2.4
+PID|||00547448^^^ST01||Tanner^Michael||19510325|M|||^^^^^^^|||||||1134733972^^^^STARACC|291442286
+PV1||Null value detected|||||11420^Eskin^Steven^J^^^MD^^^^^^STARPROV|||||||||||12|1134733972^^^^STARACC|||||||||||||||||
+ORC|RE
+OBR|||11348776858|46608^^OHHOREAP|||201112132347||||||||BLD|11420^Eskin^Steven^J^^^MD^^STARPROV||||||201112132347|||F
+OBX|1|SN|TROP^Troponin T^LA01|1|<0.010|ng/mL|0.000-0.040||||F
+NTE|1||The above 1 analytes were performed by Riverside Methodist Hospital
+NTE|2||3535 Olentangy River Rd,Columbus,OH 43214
+END
+$seg = "NTE|1||The above 1 analytes were performed by Riverside Methodist Hospital"
+$segs = "OBX||TX|APRESULT^.^LA01|1|.||||||F\nOBX||TX|BSRESULT^.^LA02|2|  REPORT||||||F"
+$field = "Thompson^Richard^L^III"
+
+require "#{proj_dir}/module/HL7"
+
+mh = HL7::MessageHandler.new( FILE, 90 )
+puts mh.records.first
