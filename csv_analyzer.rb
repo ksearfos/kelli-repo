f07ddacaad8312a166d7158703cfa471f387ac72
Tim Morgan <tmthy.morgan@gmail.com>
2014-04-18 16:45:57 -0400

    Classified csv_analyser

--- csv_analyzer.rb
+++ csv_analyzer.rb
@@ -1,38 +1,66 @@
+# This program takes a comma separated values file, or set of files,
+# and parses it to analyze its contents.
+#
+# Author:: Tim Morgan (mailto:tmorgan@manifestcorp.com)
 require "csv"
 
-HEADER_ROW = 0
+# This class provides methods necessary to parse throgh and analyze 
+# comma separated values files.
+class CSVAnalyzer
+  HEADER_ROW = 0 # The first row of a csv file contains header information
 
-def countDataColumns(row)
-  data_col_count = 0
-  row.each do |col|
-    data_col_count += 1 unless col =~ /^HAS /
+  attr_reader :csv_data, :csv_output
+  attr_accessor :ignored_column_count, :data_columns
+
+  def initialize
+    @csv_data = []
+    @csv_output = []
+    @ignored_column_count = 0
+    @data_columns = 0
   end
-  data_col_count
-end
 
-output_data = []
-first_time = true
-
-Dir.glob("./CSV/*.csv") do |csv_file|
-  results = CSV.read(csv_file)
-  if first_time
-    output_data << results[HEADER_ROW]
-    DATA_COLUMNS = countDataColumns(results[HEADER_ROW])
-    first_time = false
+  # Get a count of the columns we are NOT evaluating,
+  # regex param matches columns we ARE evaluating.
+  def countIgnoredColumns(row, regex)
+    data_col_count = 0
+    row.each { |col| data_col_count += 1 unless col =~ regex }
+    data_col_count
   end
-  results.each do |result|
-    result_columns = (result.size - DATA_COLUMNS)
-    failures = 0.0
-    result[DATA_COLUMNS..-1].each do |col|
-      failures += 1 if col.upcase == "FAILED"
+
+  # Add csv file contents from a directory to @csv_data.
+  def addToCSVDataFromDir(dir_name)
+    Dir.glob("#{dir_name}/*.csv") do |csv_file|
+      csv_file_contents = CSV.read(csv_file)
+      @csv_data += csv_file_contents
     end
-    output_data << result if (failures / result_columns) >= 0.5
   end
-end
 
-CSV.open("results_with_many_failures.csv", "wb",
-         {:force_quotes => false}) do |csv|
-  output_data.each do |row|
-    csv << row
+  # Get the header row from a csv data array.
+  def getCSVHeaderRow(csv_data_array=@csv_data)
+    csv_data_array[HEADER_ROW]
   end
+
+  def setOutputHeader(header)
+    @csv_output.insert(0, header)
+  end
+
+  def parseCSVData(header, search_term, precision)
+    @csv_data.each do |row|
+      matches = 0.0
+      result_columns = (result.size - @data_columns)
+      unless row == header
+        row[@data_colums..-1].each { |col| matches += 1 if col == search_term }
+      end
+      @csv_output << row if (matches / result_columns) >= precision
+    end
+  end
+
+  # Write @csv_output to a file, overwrites any existing file
+  # with the same name.
+  def exportCSV(file_name)
+    CSV.open(file_name, "wb", {:force_quotes => false}) do |csv|
+      @csv_output.each { |row| csv << row }
+    end
+  end
+
 end
