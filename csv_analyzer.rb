50436c96793cb833772fa7078699a69ab8620a0f
t-morgan <tmthy.morgan@gmail.com>
2014-04-18 09:22:33 -0400

    Update csv_analyzer.rb
    Fixed analyzer to do what it is supposed to do. Manually tested.

--- csv_analyzer.rb
+++ csv_analyzer.rb
@@ -2,29 +2,31 @@ require "csv"
 
 HEADER_ROW = 0
 
-def countDataColumns(row) do
+def countDataColumns(row)
   data_col_count = 0
   row.each do |col|
     data_col_count += 1 unless col =~ /^HAS /
   end
+  data_col_count
 end
 
 output_data = []
+first_time = true
 
-Dir.glob("/path/to/dir/*.csv") do |csv_file|
+Dir.glob("./CSV/*.csv") do |csv_file|
   results = CSV.read(csv_file)
-  DATA_COLUMNS = countDataColumns(results[HEADER_ROW])
+  if first_time
+	  output_data << results[HEADER_ROW]
+	  DATA_COLUMNS = countDataColumns(results[HEADER_ROW])
+	  first_time = false
+  end
   results.each do |result|
     result_columns = (result.size - DATA_COLUMNS)
-    failures = 0
-    failures += result[DATA_COLUMNS..-1].each do |col|
-      if col.upcase == "FAILED"
-        1
-      else
-        0
-      end
+    failures = 0.0
+	result[DATA_COLUMNS..-1].each do |col|
+      failures += 1 if col.upcase == "FAILED"
     end
-    output_data += result if (failures / result_columns) >= 0.5
+    output_data << result if (failures / result_columns) >= 0.5
   end
 end
 
