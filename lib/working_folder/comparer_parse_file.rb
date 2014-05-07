189750b4812007b1f055a5d4915e1655b9a7ef80
Kelli Searfos <ksearfos@manifestcorp.com>
2014-05-07 15:20:54 -0400

    *retested* removed commented out code from comparer_parse_file.rb

--- lib/working_folder/comparer_parse_file.rb
+++ lib/working_folder/comparer_parse_file.rb
@@ -4,8 +4,7 @@ TIMESTAMP = Time.now.strftime "%H%M_%m-%d-%Y"      # HHMM_MM-DD-YYYY
 
 def run_comparison(hl7_files, result_directory)
   result_file_prefix = "#{result_directory}/#{TIMESTAMP}" 
-  my_results = Hash.new(0)  #{ :number_of_records => 0, :matched_criteria => 0, :subset_size => 0 }
-  num_records = 0
+  my_results = Hash.new(0)  
 
   $logger.info "Found #{hl7_files.size} new file(s)\n"  
   until hl7_files.empty?   # I am hoping that doing it this way will clear up memory as we go along
@@ -14,33 +13,14 @@ def run_comparison(hl7_files, result_directory)
     tmp = result_file_prefix + "temp_results"
     
     runner = ComparisonRunner.new(file, tmp)
-    runner.compare
-    # file_handler = nil    # reset
-#     
-    # file_handler = get_records(file, MAX_RECS)   
-    # if file_handler.nil?   # will be nil if file was empty
-      # remove_files( [file] )   # remove even if we are testing! it's empty!!
-    # else        
-      # begin
-        # $logger.info "Found #{file_handler.size} record(s)\n" 
-        # $logger.info "Comparing records..."    
-        # num_records += file_handler.records.size  #NEW
-        # run_record_comparer( tmp, file_handler.records, false, false )
-        # file_handler.next     # get the next however-many records -- @records will be empty if we got them all
-      # end until file_handler.records.empty?
-            
-      my_results[:number_of_records] += runner.record_count
+    runner.compare            
+    my_results[:number_of_records] += runner.record_count
       
-      runner = ComparisonRunner.new(tmp, outfile)      
-      
-      # $logger.info "Found #{tmp_file_handler.size} record(s)\n" 
-      # $logger.info "Comparing records..."       
-      # criteria, subset = run_record_comparer( outfile, tmp_file_handler.records, false, true )
-      criteria, subset = runner.compare
-      my_results[:matched_criteria] = criteria  # NEW
-      my_results[:subset_size] = subset   # NEW
-      remove_files( [tmp] )  
-    # end
+    runner = ComparisonRunner.new(tmp, outfile)      
+    criteria, subset = runner.compare
+    my_results[:matched_criteria] = criteria
+    my_results[:subset_size] = subset   
+    remove_files( [tmp] )  
   end # until  
 
   $logger.info "Exiting..."
