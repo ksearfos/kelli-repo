2e9abedf908bf2bec8d6dd197c76d4484592594c
t-morgan <tmthy.morgan@gmail.com>
2014-05-14 09:56:27 -0400

    cleaning up core of cucumber setup

--- epic_cucumber/features/pages/careconnect_login_page.rb
+++ epic_cucumber/features/pages/careconnect_login_page.rb
@@ -1,23 +1,23 @@
 class CareConnectLoginPage
   attr_accessor :user_name, :password, :log_on_button
 
-  URLS = { :login => "http://ccweb" }
+  URL = "http://ccweb"
 
   def initialize(browser)
     @browser = browser
-    @user_name = @browser.text_field(:name => "user")
+    @username = @browser.text_field(:name => "user")
     @password = @browser.text_field(:name => "password")
     @log_on_button = @browser.link(:name => "btnLogin")
   end
 
   def visit
-    @browser.goto URLS[:login]
+    @browser.goto URL
   end
 
   def login_with(username, password)
-    self.user_name.set username
-    self.password.set password
-    self.log_on_button.click
+    @username.set username
+    @password.set password
+    @log_on_button.click
     ccweb_landing_page = CareConnectLandingPage.new(@browser)
     ccweb_landing_page.username.wait_until_present if WEBDRIVER
     ccweb_landing_page
--- epic_cucumber/features/support/env.rb
+++ epic_cucumber/features/support/env.rb
@@ -1,4 +1,16 @@
+require 'io/console'
+require 'rautomation'
+
 TEST_DATA_DIR = "./features/test_data"
+
+# Enter credentials to log in
+print "OPID:   "
+$stdout.flush
+username = gets.chomp
+print "Password:   "
+$stdout.flush
+password = STDIN.noecho(&:gets).chomp
+print "\n" # Move console output off password line
  
 if ENV["HEADLESS"] then
   require "celerity"
@@ -13,30 +25,23 @@ else
 end
  
 Before do
+  @username = username
+  @password = password
   @browser = browser
   # Navigate to the CareConnect web page
   @ccweb_login_page = CareConnectLoginPage.new(@browser)
   @ccweb_login_page.visit
-
-  # Enter credentials to log in
-  print "OPID:   "
-  $stdout.flush
-  @username = gets.chomp
-  print "Password:   "
-  $stdout.flush
-  @password = STDIN.noecho(&:gets).chomp
-  print "\n" # Move console output off password line
-
+  
   # Log in and open Epic
   @ccweb_landing_page = @ccweb_login_page.login_with @username, @password
   @ccweb_landing_page.epic_link.click
   sleep 15
   window = RAutomation::Window.new(:title => /Hyperspace/i, :adapter => :ms_uia)
-  window.exists?.should be_true
+  window.exists?.should be_true # Quit if Epic does not open
   window.send_keys @username
   window.move_mouse 400, 375
   window.click_mouse
-  @password.scan(/./).each do |c|
+  @password.scan(/./).each do |c| # Handle some special characters
     c = :add if c == "+"
     window.send_keys c
   end
